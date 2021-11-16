7390a97f75895a7df2c10a45ff8b39dd09f2c810
Sergio Medina <sme@monsido.com>
2021-11-16 07:52:26 +0100

    ruby 3 support

--- lib/activerecord-native_db_types_override/patches.rb
+++ lib/activerecord-native_db_types_override/patches.rb
@@ -3,13 +3,13 @@ module ActiveRecord
     module SchemaStatements
       alias_method :add_reference_without_native_db_types_override, :add_reference
 
-      def add_reference(table_name, ref_name, options = {})
+      def add_reference(table_name, ref_name, **options)
         if ::NativeDbTypesOverride.migrations_options
           add_ref_opts = ::NativeDbTypesOverride.migrations_options[:add_reference]
           options = Marshal.load(Marshal.dump(add_ref_opts)).merge(options) if add_ref_opts
         end
 
-        add_reference_without_native_db_types_override(table_name, ref_name, options)
+        add_reference_without_native_db_types_override(table_name, ref_name, **options)
       end
     end
   end
